d0975fc9208a27893ca9b381c44a903aefd828cc
Hunter McGarity <68818145+McGarity99@users.noreply.github.com>
2022-06-24 13:58:17 -0400

    Score Work
    -NOTE: Unstable Build
    -commit to move work over to new machine
    -additional work on score mechanism, attempting to add score values to enemies

--- src/systems/combat.rs
+++ src/systems/combat.rs
@@ -3,6 +3,8 @@ use crate::prelude::*;
 #[system]
 #[read_component(WantsToAttack)]
 #[read_component(Player)]
+#[read_component(ProvidesScore)]
+#[write_component(Score)]
 #[write_component(Health)]
 #[write_component(Armor)]
 #[read_component(Damage)]
@@ -11,6 +13,9 @@ pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
     let mut attackers = <(Entity, &WantsToAttack)>::query();
     let mut take_health = false;
     let mut health_damage = 0;
+
+    /* let mut score_query = <&Score>::query().filter(component::<Player>());
+    let mut player_score = score_query.iter(ecs).nth(0).unwrap(); */
 
     let victims: Vec<(Entity, Entity, Entity)> = attackers
         .iter(ecs)
@@ -33,29 +38,29 @@ pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
         } else {
             0
         };
-        let weapon_damage: i32 = <(&Carried, &Damage)>::query().iter(ecs)
+        let weapon_damage: i32 = <(&Carried, &Damage)>::query()
+            .iter(ecs)
             .filter(|(carried, _)| carried.0 == *attacker)
             .map(|(_, dmg)| dmg.0)
             .sum();
         let final_damage = base_damage + weapon_damage;
-        if let Ok(mut armor) = ecs
-            .entry_mut(*victim)
-            .unwrap()
-            .get_component_mut::<Armor>()
-        {
+        if let Ok(mut armor) = ecs.entry_mut(*victim).unwrap().get_component_mut::<Armor>() {
             if armor.current > 0 {
                 println!("damage received: {}", final_damage);
-                let new_damage = final_damage - armor.current;      //calculate damage taken by armor
-                if new_damage < 0 {                                      //if armor absorbs all damage with armor points left over
+                let new_damage = final_damage - armor.current; //calculate damage taken by armor
+                if new_damage < 0 {
+                    //if armor absorbs all damage with armor points left over
                     armor.current = new_damage * -1;
-                } else if new_damage == 0 {                              //if armor absorbs all damage with no armor points left over
+                } else if new_damage == 0 {
+                    //if armor absorbs all damage with no armor points left over
                     armor.current = 0;
-                } else {                                                 //if damage is enough to "break" armor and damage player's health
+                } else {
+                    //if damage is enough to "break" armor and damage player's health
                     take_health = true;
                     health_damage = new_damage;
                 }
             } else {
-                take_health = true;         //move to take away health if armor is 0
+                take_health = true; //move to take away health if armor is 0
                 health_damage = final_damage;
             }
         } else {
@@ -66,23 +71,45 @@ pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
             .unwrap()
             .get_component_mut::<Health>()
         {
-            if is_player && take_health {               //if player is attacked (and no armor or broken armor)
+            if is_player && take_health {
+                //if player is attacked (and no armor or broken armor)
                 health.current -= health_damage;
-            } else if !is_player {                      //if monster is attacked (apply player's final_damage)
+            } else if !is_player {
+                //if monster is attacked (apply player's final_damage)
                 health.current -= final_damage;
             }
             if health.current < 1 && !is_player {
+                if let Ok(ProvidesScore) = ecs
+                    .entry_mut(*victim)
+                    .unwrap()
+                    .get_component::<ProvidesScore>()
+                {
+                    let score_yield = ecs
+                        .entry_ref(*victim)
+                        .unwrap()
+                        .get_component::<ProvidesScore>();
+                    if let Ok(s_y) = score_yield {
+                        let mut score_query = <&Score>::query().filter(component::<Player>());
+                        let mut player_score = score_query.iter(ecs).nth(0).unwrap();
+                        player_score.current = i32::min(player_score.max, player_score.current + s_y.amount);
+                    }
+                    /* match score_yield {
+                        Ok(s_y) => {
+                            player_score.current += i32::min(player_score.max, player_score.current + s_y.amount);
+                        },
+                        _ => {}
+                    } */
+                }
                 commands.remove(*victim);
             }
         }
-        
-        
+
         /* if let Ok(mut health) = ecs
             .entry_mut(*victim)
             .unwrap()
             .get_component_mut::<Health>()
         {
-            
+
             println!("combat.rs Health before attack: {}", health.current);
             health.current -= 1;
             if health.current < 1 && !is_player {
--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -27,6 +27,7 @@ pub fn player_input(
             VirtualKeyCode::Down => Point::new(0, 1),
             VirtualKeyCode::G => {
                 let mut picked_up_gold = false; //flag for whether the player picked up gold or some other item
+                let mut score_amt = 0i32;   //temp variable to store the amount added to the player's score from ScoreItem
 
                 //let mut score_to_apply = Vec::<(Entity, i32)>::new();
 
@@ -34,24 +35,28 @@ pub fn player_input(
                     .iter(ecs)
                     .find_map(|(entity, pos)| Some((*entity, *pos)))
                     .unwrap();
+                
                 let mut player_query = <Entity>::query().filter(component::<Player>()); //get the score (only the player has a Score component)
-                let mut player_entity = player_query.iter(ecs).nth(0).unwrap();
+                let mut player_entity = player_query.iter(ecs).nth(0).unwrap(); //get player so we can access their Score component
 
-                let mut gold_locs = <(&ScoreItem, &Point)>::query();
+                let mut gold_locs = <(Entity, &ScoreItem, &Point, &ProvidesScore)>::query();   //get all entities that have a ScoreItem & Point component
                 gold_locs
                     .iter(ecs)
-                    .filter(|(s_i, &pos)| pos == player_pos)
-                    .for_each(|(item, position)| {
+                    .filter(|(_entity, s_i, &pos, &p_s)| pos == player_pos)
+                    .for_each(|(entity, s_item, position, provides_score)| {
                         println!("gold at pos: {:?}", position);
                         picked_up_gold = true;
-                    });
+                        score_amt += provides_score.amount;
+                        commands.remove_component::<Point>(*entity);
+                    }); //iterate over all found entities, filter out all that are not at the same position as the player
+                        //for each matching entity (max is 1), set the flag to true and remove it from the game world
 
                 if picked_up_gold {
                     if let Ok(mut score) = ecs.entry_mut(*player_entity)
                         .unwrap()
                         .get_component_mut::<Score>()
                     {
-                        score.current += 100;
+                        score.current += score_amt;
                     }
                 } else {
                     /* gold_locs.iter(ecs)
